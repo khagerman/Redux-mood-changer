33ee5df3e34b69686ae0c5135ddf2e47917c8f4a
khagerman <71734063+khagerman@users.noreply.github.com>
2021-09-29 15:09:36 -0500

    made switch

---
+++ redux.js
@@ -0,0 +1,19 @@
+const INITIAL_STATE = { face: "ఠ_ఠ" };
+const moodChanger = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "HAPPY":
+      return { ...state, face: "(◕‿◕)" };
+    case "ANNOYED":
+      return { ...state, face: "◔_◔" };
+    case "ANGRY":
+      return { ...state, face: "ಠ_ಠ" };
+    case "OVERJOYED":
+      return { ...state, face: "＼(＾O＾)／" };
+    case "IN_LOVE":
+      return { ...state, face: "✿♥‿♥✿" };
+
+    default:
+      return state;
+  }
+};
+const store = Redux.createStore(moodChanger);
